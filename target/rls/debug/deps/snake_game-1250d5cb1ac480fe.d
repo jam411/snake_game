9d3e7b940eb844033caa7bc55d403a303e9d14c9
Ryoji Simizu <ryojisimizu@wl206167.jaist.ac.jp>
2021-06-20 04:05:44 +0900

    first goal

---
+++ target/rls/debug/deps/snake_game-1250d5cb1ac480fe.d
@@ -0,0 +1,5 @@
+/Users/ryojisimizu/rust/snake_game/target/rls/debug/deps/snake_game-1250d5cb1ac480fe.rmeta: src/main.rs
+
+/Users/ryojisimizu/rust/snake_game/target/rls/debug/deps/snake_game-1250d5cb1ac480fe.d: src/main.rs
+
+src/main.rs:
---
+++ target/rls/debug/deps/snake_game-179fc90db5108b30.d
@@ -0,0 +1,5 @@
+/Users/ryojisimizu/rust/snake_game/target/rls/debug/deps/snake_game-179fc90db5108b30.rmeta: src/main.rs
+
+/Users/ryojisimizu/rust/snake_game/target/rls/debug/deps/snake_game-179fc90db5108b30.d: src/main.rs
+
+src/main.rs:
