53bda5c280f9e5646ffa310e84d0bedf9bae9dba
Ryoji Simizu <ryojisimizu@wl206167.jaist.ac.jp>
2021-06-20 01:55:10 +0900

    Updating to show yellow point snake

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use opengl_graphics::{GlGraphics, OpenGL};
 
 struct Game {
     gl: GlGraphics,
+    snake: Snake,
 }
 
 impl Game {
@@ -21,6 +22,27 @@ impl Game {
 
         self.gl.draw(arg.viewport(), |_c, gl| {
             graphics::clear(sky_blue, gl);
+        });
+
+        self.snake.render(&mut self.gl, arg);
+    }
+}
+
+struct Snake {
+    pos_x: i32,
+    pos_y: i32,
+}
+
+impl Snake {
+    fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
+        let yellow: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let square = graphics::rectangle::square(self.pos_x as f64, self.pos_y as f64, 20_f64);
+
+        gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform;
+
+            graphics::rectangle(yellow, square, transform, gl);
         });
     }
 }
@@ -38,7 +60,8 @@ fn main() {
         
 
     let mut game = Game {
-        gl: GlGraphics::new(opengl)
+        gl: GlGraphics::new(opengl),
+        snake: Snake { pos_x: 50, pos_y: 100 },
     };
 
     let mut events = Events::new(EventSettings::new());
