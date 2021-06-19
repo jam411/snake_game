ce050ae02e9ade567149e4a6c718b183fbda89bc
Ryoji Simizu <ryojisimizu@wl206167.jaist.ac.jp>
2021-06-20 02:19:09 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use piston::input::*;
 use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
 
-
+use std::collections::LinkedList;
 
 #[derive(Clone, PartialEq)]
 enum Direction {
@@ -56,31 +56,44 @@ impl Game {
 }
 
 struct Snake {
-    pos_x: i32,
-    pos_y: i32,
+    body: LinkedList<(i32, i32)>,
     dir: Direction,
 }
 
 impl Snake {
     fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
-        let yellow: [f32; 4] = [1.0, 0.0, 0.0, 0.0];
+        let yellow: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
-        let square = graphics::rectangle::square((self.pos_x * 20) as f64, (self.pos_y * 20) as f64, 20_f64);
+        let square: Vec<graphics::types::Rectangle> = self.body
+            .iter()
+            .map(|&(x, y)| {
+                graphics::rectangle::square(
+                    (x * 20) as f64,
+                    (y * 20) as f64,
+                    20_f64)
+            })
+            .collect();
 
         gl.draw(args.viewport(), |c, gl| {
             let transform = c.transform;
-
-            graphics::rectangle(yellow, square, transform, gl);
+            squares.into_iter()
+                .for_each(|square| graphics::rectangle(yellow, square, transform, gl));
         });
     }
 
     fn update(&mut self) {
+        let mut new_head = (*self.body.front().expect("Snake has no body")).clone();
+
         match self.dir {
-            Direction::Left => self.pos_x -= 1,
-            Direction::Right => self.pos_x += 1,
-            Direction::Up => self.pos_y -= 1,
-            Direction::Down => self.pos_y += 1,
+            Direction::Left => new_head.0 -= 1,
+            Direction::Right => new_head.0 += 1,
+            Direction::Up => new_head.1 -= 1,
+            Direction::Down => new_head.1 += 1,
         }
+
+        self.body.push_front(new_head);
+
+        self.body.pop_back().unwrap();
     }
 }
 
@@ -98,7 +111,7 @@ fn main() {
 
     let mut game = Game {
         gl: GlGraphics::new(opengl),
-        snake: Snake { pos_x: 9, pos_y: 9, dir: Direction::Right },
+        snake: Snake { dir: Direction::Right },
     };
 
     let mut events = Events::new(EventSettings::new()).ups(8);
